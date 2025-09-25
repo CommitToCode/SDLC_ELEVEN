16a1875ca446a96a6155eb02b640208f503bd45b
CommitToCode <rishi2020.rp@gmail.com>
2025-09-25 20:01:35 +0530

    updated

--- app/models/otpModel.js
+++ app/models/otpModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Defining Schema
 const otpverifySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   otp: { type: String, required: true },
   createdAt: { type: Date, default: Date.now, expires: "15m" },
 });
